cd55d8952a396bd2ae9c47f911fe16794fc80fcf
Calvin Kim <calvinskim@gmail.com>
2022-07-30 08:50:46 -0700

    unzip, extract from xml, and parse text into dict

--- parser.py
+++ parser.py
@@ -1,9 +1,16 @@
 # Parse text from ODP file
+# text:p "P1" = song title
+# text:p "P5" = verse
+# text:p "P3" = lyrics
+# text:span "T1" = song title (first page)
+# text:span "T4" = song title
+# text:span "T3" = verse
+# text:span "T2" = lyrics
 
-import zipfile
+import zipfile, json
 from xml.dom import minidom
 
-tmpfile = '/home/calvin/Documents/sdic/backups/sdic-backup_2022-01-16_1114_full/sdi.church/Worship Presentation/ODP/Praises/Joy.odp'
+tmpfile = '/home/calvin/Documents/sdic/backups/sdic-backup_2022-04-30_1735_full/sdi.church/Worship Presentation/ODP/Praises/Agnus Dei.odp'
 targetFile = 'content.xml'
 
 def unzip(file):
@@ -18,36 +25,129 @@ def unzip(file):
         print('File is not a zip file')
         print(tmpfile)
 
-def parseText(file):
-    parser = minidom.parse(file)
-    slides = parser.getElementsByTagName('draw:page')
-    
-    # for each page/slide
-    for slide in slides:
-        slideNo = slide.attributes['draw:name'].value
-        print(slideNo)
+def load_ODP(file):
+    return minidom.parse(file)
 
-        # get all texts in a slide
-        spanTags = slide.getElementsByTagName('text:span')
+def get_slides(obj):
+    return obj.getElementsByTagName('draw:page')
+
+def get_textPs(slide):
+    # get all text:p(text box) in a slide
+    return slide.getElementsByTagName('text:p')
+
+def get_text(textP):
+    text = []
+    #nodeList => text:span
+    nodeList = textP.childNodes
+    line = ''
+    for node in nodeList:
+        element = node.firstChild
+        if element.nodeType == element.TEXT_NODE:
+            # if a song line is spread to multiple lines with span, gather them into one line
+            line += element.data
+        else:
+            text.append(line)
+            line = ''
+    text.append(line)
+    return text
+
+def get_verseNo(verse):
+    str = ''.join(verse).lower()
+    switch = {
+        'verse 1'      : 'v1',
+        'verse 2'      : 'v2',
+        'verse 3'      : 'v3',
+        'verse 4'      : 'v4',
+        'verse 5'      : 'v5',
+        'pre-chorus'   : 'pc',
+        'pre chorus'   : 'pc',
+        'pre-chorus 2' : 'pc2',
+        'pre chorus 2' : 'pc2',
+        'pre-chorus2'  : 'pc2',
+        'pre chorus2'  : 'pc2',
+        'chorus'       : 'c',
+        'chorus 1'     : 'c',
+        'chorus1'      : 'c',
+        'chorus 2'     : 'c2',
+        'chorus2'      : 'c2',
+        'bridge'       : 'b',
+        'ending'       : 'e'
+    }
+    return switch.get(str, 'Invalid input')
+
+def extract_song(slides):
+    data = {
+        'title' : '',
+        'slides': {}
+    }
+    for slide in slides:
+        songTitle = []
+        verse = []
+        lyric = []
+        textPs = get_textPs(slide)
+
+        if textPs.length == 0:
+            # No text <p> element. This is last slide which is usually empty
+            continue
+
+        slideNo = slide.attributes['draw:name'].value[4:]
+
+        for textP in textPs:
+            if slideNo == 'page1':
+                if textP.attributes['text:style-name'].value == 'P1':
+                    # this is song title; do this only on 'page1'
+                    songTitle = get_text(textP)
+                    data['title'] = ''.join(songTitle)
+            if textP.attributes['text:style-name'].value == 'P3':
+                # this is lyric
+                lyric += get_text(textP)
+            elif textP.attributes['text:style-name'].value == 'P5':
+                # this is verse
+                verse = get_text(textP)
         
-        texts = []
-        # for each textline in a slide
-        for span in spanTags:
-            if span.hasAttribute('text:style-name'):
-                if span.attributes['text:style-name'].value == 'T1':
-                    songTitle = span.firstChild.data
-                elif span.attributes['text:style-name'].value == 'T2':
-                    texts.append(span.firstChild.data)
-                elif span.attributes['text:style-name'].value == 'T3':
-                    verse = span.firstChild.data
-        if 'songTitle' in locals():
-            lyrics = "\n".join(texts)
-            print(songTitle)
-            print(verse)
-            print(lyrics)
-            print('----')
-                
+        verseNo = get_verseNo(verse)
+        data['slides'][slideNo] = {
+            verseNo: lyric
+        }
+    return data
+    
 
 if __name__ == '__main__':
     #unzip(tmpfile)
-    parseText(targetFile)
+    odpObj = load_ODP(targetFile)
+    slides = get_slides(odpObj)
+    song = extract_song(slides)
+
+    print(json.dumps(song, indent=4))
+
+
+        
+    # sample data structure
+    song1 = {
+        'title': 'Alleluia',
+        'slides': {
+            'page1': { 'verse1': [ 'Line1', 'Line2', 'Line3', 'Line4' ], },
+            'page2': { 'verse1': [ 'Line1', 'Line2', 'Line3' ], },
+            'page3': { 'chorus': [ 'Line1', 'Line2', 'Line3' ], },
+            'page4': { 'chorus': [ 'Line1', 'Line2', 'Line3' ], },
+            'page5': { 'verse2': [ 'Line1', 'Line2', 'Line3' ], },
+            'page6': { 'verse2': [ 'Line1', 'Line2', 'Line3' ], }
+        }
+    }
+    song2 = {
+        'title': 'Alleluia',
+        'slides': {
+            1:  { 'v1': [ 'Line1', 'Line2', 'Line3', 'Line4' ], },
+            2:  { 'v1': [ 'Line1', 'Line2', 'Line3' ], },
+            3:  { 'pc': [ 'Line1', 'Line2', 'Line3' ], },
+            4:  { 'pc': [ 'Line1', 'Line2', 'Line3' ], },
+            5:  { 'c':  [ 'Line1', 'Line2', 'Line3' ], },
+            6:  { 'c':  [ 'Line1', 'Line2', 'Line3' ], },
+            7:  { 'v2': [ 'Line1', 'Line2', 'Line3' ], },
+            8:  { 'v2': [ 'Line1', 'Line2', 'Line3' ], },
+            9:  { 'c2': [ 'Line1', 'Line2', 'Line3' ], },
+            10: { 'c2': [ 'Line1', 'Line2', 'Line3' ], },
+            11: { 'b':  [ 'Line1', 'Line2', 'Line3' ], },
+            12: { 'b':  [ 'Line1', 'Line2', 'Line3' ], }
+        }
+    }
