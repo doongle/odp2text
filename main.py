f21ddc3c69d456b025d18f34c0d0bb0bc6b917dc
Calvin Kim <calvinskim@gmail.com>
2022-07-31 12:52:27 -0700

    get contents from song directory

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-import parser
+import parser, glob, zipfile
 
-song_directory = ''
+song_directory = './Praises/'
+
+ODPs = glob.glob(song_directory + "*.odp")
+
+def unzip2(file):
+    targetFile = 'content.xmls'
+    with zipfile.ZipFile(file, 'r') as zipObj:
+        target = zipObj.getinfo(targetFile)
+        print(target)
+        # print(file + '-' + target.filename)
+        # target.filename = file.replace(' ', '_') + '-' + target.filename
+        # zipObj.extract(target)
+
+
+if __name__ == '__main__':
+    # print(ODPs)
+    unzip2('Agnus Dei.odp')
